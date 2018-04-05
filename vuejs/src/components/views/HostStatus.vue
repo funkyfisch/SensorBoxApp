23a204795f45ebcf68c306bb9f890a422f08bddc
funkyfisch <funkyfisch@gmail.com>
2018-04-06 00:28:58 +0200

    Fixed responsiveness in host status page

--- vuejs/src/components/views/HostStatus.vue
+++ vuejs/src/components/views/HostStatus.vue
@@ -2,14 +2,15 @@
   <div>
     <div class="jumbotron">
       <div class="row">
-        <div class="col-lg-4 col-md-4">
+
+        <div class="col-lg-4 offset-lg-0 col-md-10 offset-md-1 col-sm-12">
           <div class="card">
             <div class="card-header">
               <h4 class="card-title"> Current Load </h4>
             </div>
             <div class="card-body">
               <div class="row">
-                <div class="col-lg-6 offset-lg-3 col-md-6 offset-md-3">
+                <div class="col-lg-6 offset-lg-3 col-sm-12">
                   <h5> Average Load </h5>
                   <h3> {{this.cpuLoad}} %</h3>
                 </div>
@@ -17,7 +18,7 @@
             </div>
             <div class="card-footer">
               <div v-if="this.cpus.length > 0" class="row">
-                <div v-for="cpu, index in this.cpus" class="col-lg-3 col-md-3">
+                <div v-for="cpu, index in this.cpus" class="col-lg-3 col-sm-3">
                   <h6> core #{{index+1}} </h6>
                   <h6> {{cpu.load.toFixed(2)}} </h6>
                 </div>
@@ -25,14 +26,15 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4">
+
+        <div class="col-lg-4 offset-lg-0 col-md-10 offset-md-1 col-sm-12">
           <div class="card">
             <div class="card-header">
               <h4 class="card-title"> Storage </h4>
             </div>
             <div class="card-body">
               <div class="row">
-                <div class="col-lg-6 offset-lg-3 col-md-6 offset-md-3">
+                <div class="col-lg-6 offset-lg-3 col-sm-12">
                   <h5> Used / Total (GB) </h5>
                   <h3> {{this.totalStorageUsed}} / {{this.totalStorageSize}}</h3>
                 </div>
@@ -40,7 +42,7 @@
             </div>
             <div class="card-footer">
               <div v-if="this.cpus.length > 0" class="row">
-                <div v-for="dev, index in this.storageDevices" class="col-lg-3 col-md-3">
+                <div v-for="dev, index in this.storageDevices" class="col-lg-3 col-sm-4">
                   <h6> {{dev.fs}} </h6>
                   <h6> {{dev.used}} / {{dev.size}} </h6>
                 </div>
@@ -48,14 +50,15 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4">
+
+        <div class="col-lg-4 offset-lg-0 col-md-10 offset-md-1 col-sm-12">
           <div class="card">
             <div class="card-header">
               <h4 class="card-title"> RAM Usage </h4>
             </div>
             <div class="card-body">
               <div class="row">
-                <div class="col-lg-6 offset-lg-3 col-md-6 offset-md-3">
+                <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3">
                   <h5> Used / Total (MB)</h5>
                   <h3> {{this.memory.used}} / {{this.memory.total}}</h3>
                 </div>
@@ -63,15 +66,15 @@
             </div>
             <div class="card-footer">
               <div class="row">
-                <div class="col-lg-4 col-md-4">
+                <div class="col-lg-4 col-sm-4">
                   <h6> Swap Total </h6>
                   <h6> {{this.memory.swaptotal}} MB</h6>
                 </div>
-                <div class="col-lg-4 col-md-4">
+                <div class="col-lg-4 col-sm-4">
                   <h6> Swap Used </h6>
                   <h6> {{this.memory.swapused}} MB</h6>
                 </div>
-                <div class="col-lg-4 col-md-4">
+                <div class="col-lg-4 col-sm-4">
                   <h6 class="primary"> Swap Free </h6>
                   <h6> {{this.memory.swapfree}} MB</h6>
                 </div>
@@ -79,6 +82,7 @@
             </div>
           </div>
         </div>
+
       </div>
       <div class="row">
       </div>
